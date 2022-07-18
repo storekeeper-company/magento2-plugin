26eaff8046cbe2d7329007828f04296fbb9dbb43
Rick Daalhuizen <r.daalhuizen@creative-ct.nl>
2022-07-18 16:33:26 +0200

    STORE-28

---
+++ Block/Adminhtml/Form/Field/ShopId.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace StoreKeeper\StoreKeeper\Block\Adminhtml\Form\Field;
+
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Data\Form;
+use Magento\Framework\Data\Form\AbstractForm;
+use Magento\Framework\Data\Form\Element\Renderer\RendererInterface;
+use Magento\Framework\Escaper;
+use Magento\Framework\Math\Random;
+use Magento\Framework\View\Helper\SecureHtmlRenderer;
+
+use Magento\Framework\Data\Form\Element\Factory;
+use Magento\Framework\Data\Form\Element\CollectionFactory;
+
+use StoreKeeper\StoreKeeper\Helper\Api\Auth;
+
+class ShopId extends \Magento\Framework\Data\Form\Element\AbstractElement
+{
+    public function __construct(
+        Factory $factoryElement,
+        CollectionFactory $factoryCollection,
+        Escaper $escaper,
+        ?SecureHtmlRenderer $secureRenderer,
+        ?Random $random,
+        Auth $authHelper,
+        \Magento\Framework\App\Request\Http $request,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        $data = []
+    ) {
+        parent::__construct($factoryElement, $factoryCollection, $escaper, $data, $secureRenderer, $random);
+        $this->authHelper = $authHelper;
+        $this->request = $request;
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    public function getElementHtml()
+    {
+        $storeInformation = json_decode($this->scopeConfig->getValue(
+            'storekeeper_general/general/storekeeper_store_information',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORES,
+            $this->request->getParam('store')
+        ), true);
+
+        $shopId = $storeInformation['shop']['id'];
+
+        return "<input type='text' class='input-text admin__control-text' value='{$shopId}' readonly />";
+    }
+}
