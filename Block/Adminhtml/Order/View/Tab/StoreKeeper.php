8b3bb509cd0e84699624e907a05a6d5b3fc262c1
tpalstra <t.palstra@creative-ct.nl>
2022-11-29 15:26:19 +0100

    Removed args

--- Block/Adminhtml/Order/View/Tab/StoreKeeper.php
+++ Block/Adminhtml/Order/View/Tab/StoreKeeper.php
@@ -14,17 +14,9 @@ namespace StoreKeeper\StoreKeeper\Block\Adminhtml\Order\View\Tab;
 class StoreKeeper extends \Magento\Backend\Block\Template implements \Magento\Backend\Block\Widget\Tab\TabInterface
 {
     protected $_template = "StoreKeeper_StoreKeeper::storekeeper/order/view/tab/storekeeper.phtml";
-    /**
-     * @var \Magento\Framework\Registry
-     */
+
     private $_coreRegistry;
  
-    /**
-     * View constructor.
-     * @param \Magento\Backend\Block\Template\Context $context
-     * @param \Magento\Framework\Registry $registry
-     * @param array $data
-     */
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
         \Magento\Framework\Registry $registry,
@@ -44,62 +36,36 @@ class StoreKeeper extends \Magento\Backend\Block\Template implements \Magento\Ba
         return null;
     }
  
-    /**
-     * Retrieve order model instance
-     * 
-     * @return \Magento\Sales\Model\Order
-     */
     public function getOrder()
     {
         return $this->_coreRegistry->registry('current_order');
     }
-    /**
-     * Retrieve order model instance
-     *
-     * @return int
-     *Get current id order
-     */
+
     public function getOrderId()
     {
         return $this->getOrder()->getEntityId();
     }
  
-    /**
-     * Retrieve order increment id
-     *
-     * @return string
-     */
     public function getOrderIncrementId()
     {
         return $this->getOrder()->getIncrementId();
     }
-    /**
-     * {@inheritdoc}
-     */
+
     public function getTabLabel()
     {
         return __('Storekeeper');
     }
  
-    /**
-     * {@inheritdoc}
-     */
     public function getTabTitle()
     {
         return __('My Custom Tab');
     }
  
-    /**
-     * {@inheritdoc}
-     */
     public function canShowTab()
     {
         return true;
     }
  
-    /**
-     * {@inheritdoc}
-     */
     public function isHidden()
     {
         return false;
