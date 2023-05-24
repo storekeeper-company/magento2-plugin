17d45c3aeb22d25e04880654e02df46d842c5cda
Eugene <eugene@storekeeper.nl>
2023-05-24 16:29:31 +0300

    861mkv7wf Extended intergation tests: made refactoring and covered by tests (for both guest and existed customer) order with SK payment creation and order refund

--- Test/Integration/OrderRefundGuestTest.php
+++ Test/Integration/OrderRefundGuestTest.php
@@ -3,19 +3,15 @@
 namespace StoreKeeper\StoreKeeper\Test\Integration;
 
 use StoreKeeper\StoreKeeper\Test\Integration\AbstractTest;
-use Magento\TestFramework\Helper\Bootstrap;
-use Magento\Sales\Api\OrderRepositoryInterface;
-use Magento\Customer\Model\Customer;
-use Magento\Sales\Model\Order\Payment;
-use Magento\Sales\Model\Order\Item;
-use Magento\Sales\Model\Order;
-use Magento\Store\Model\StoreManagerInterface;
 
-class OrderCreationTest extends AbstractTest
+class OrderRefundGuestTest extends AbstractGuestTest
 {
     protected function setUp(): void
     {
         parent::setUp();
+        $this->paymentApiClientMock->expects($this->once())
+            ->method('attachPaymentIdsToOrder')
+            ->with('1', 55, [33]);
     }
 
     /**
@@ -24,18 +20,10 @@ class OrderCreationTest extends AbstractTest
      * @magentoConfigFixture current_store storekeeper_general/general/enabled 1
      * @magentoConfigFixture current_store storekeeper_general/general/storekeeper_sync_auth {"rights":"subuser","mode":"apikey","account":"centroitbv","subaccount":"64537ca6-18ae-41e5-a6a9-20b803f97117","user":"sync","apikey":"REDACTED"}
      * @magentoConfigFixture current_store storekeeper_general/general/storekeeper_sync_mode 4
+     * @magentoConfigFixture current_store payment/storekeeper_payment_ideal/active 1
      */
-    public function testOrderCreation()
+    public function testGuestRefund()
     {
-        $customer = $this->getCustomer();
-        $orderData = $this->getOrderData();
-        $order = $this->createOrder(
-            $orderData['billingAddress'],
-            $orderData['shippingAddress'],
-            $customer,
-            $orderData['payment'],
-            $orderData['orderItem']
-        );
-        $this->assertOrderCreation($order);
+        $this->executeRefundOrderWithPayment(true);
     }
 }
