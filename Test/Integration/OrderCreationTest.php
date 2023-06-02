38c81071263c49f62a4d938245fa60fe990ff62e
RaymondStorekeeper <133241525+RaymondStorekeeper@users.noreply.github.com>
2023-06-02 11:13:24 +0200

    Update OrderCreationTest.php
    added explanation

--- Test/Integration/OrderCreationTest.php
+++ Test/Integration/OrderCreationTest.php
@@ -24,7 +24,12 @@ class OrderCreationTest extends AbstractTest
      * @magentoConfigFixture current_store storekeeper_general/general/enabled 1
      * @magentoConfigFixture current_store storekeeper_general/general/storekeeper_sync_auth {"rights":"subuser","mode":"apikey","account":"centroitbv","subaccount":"64537ca6-18ae-41e5-a6a9-20b803f97117","user":"sync","apikey":"REDACTED"}
      * @magentoConfigFixture current_store storekeeper_general/general/storekeeper_sync_mode 4
-     */
+     
+     *The function 'testOrderCreation()', uses certain fixtures to get a basic customer, product, and order with respecting data for testing, it pulls it from the '$this' variable.
+     *afterwards, the variable '$order' is filled with the 'createOrder()' function, which uses data from the '$orderdata' variable
+     *then it runs the actual test function called 'assertOrderCreation($order)'
+    */
+    
     public function testOrderCreation()
     {
         $customer = $this->getCustomer();
