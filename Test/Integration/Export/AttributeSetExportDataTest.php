4b12fa238f751ae13f2c7c0e4503d1f612c2c2eb
Telepatrick <eugenekobiliatsky@gmail.com>
2024-04-17 12:25:29 +0300

    869487hd6 Implemneted Integration test for 'Attribute Set' entity export

---
+++ Test/Integration/Export/AttributeSetExportDataTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace StoreKeeper\StoreKeeper\Test\Integration\Export;
+
+use Magento\Eav\Model\ResourceModel\Entity\Attribute\Set\CollectionFactory;
+use Magento\TestFramework\Helper\Bootstrap;
+use StoreKeeper\StoreKeeper\Model\Export\AttributeSetExportManager;
+use StoreKeeper\StoreKeeper\Test\Integration\AbstractTest;
+
+/**
+ * @magentoDbIsolation enabled
+ */
+class AttributeSetExportDataTest extends AbstractTest
+{
+    protected $attributeSetExportManager;
+    protected $attributeSetCollectionFactory;
+
+    protected function setUp(): void
+    {
+        $this->attributeSetCollectionFactory = Bootstrap::getObjectManager()->create(CollectionFactory::class);
+        $this->attributeSetExportManager = Bootstrap::getObjectManager()->create(AttributeSetExportManager::class);
+    }
+
+    /**
+     * @magentoConfigFixture current_store storekeeper_general/general/storekeeper_shop_language nl
+     * @magentoConfigFixture current_store general/locale/code nl_NL
+     */
+    public function testGetAttributeSetExportData()
+    {
+        $this->assertEquals($this->getTestAttributeSetExportData(), $this->getAttributeSetExportData());
+    }
+
+    /**
+     * @return array
+     */
+    public function getAttributeSetExportData(): array
+    {
+        $attributeSetCollection = $this->attributeSetCollectionFactory->create();
+        $attributeSets = $attributeSetCollection->addFieldToSelect('*')->getItems();
+        $attributeSetExportData = $this->attributeSetExportManager->getAttributeSetExportData($attributeSets);
+
+        return $this->getFoundEntityData('Default', $attributeSetExportData, 'path://name');
+    }
+
+    /**
+     * @return array
+     */
+    public function getTestAttributeSetExportData(): array
+    {
+        return [
+            'path://name' => 'Default',
+            'path://alias' => NULL,
+            'path://translatable.lang' => 'nl',
+            'path://is_main_lang' => 'yes',
+            'path://published' => 'true'
+        ];
+    }
+}
