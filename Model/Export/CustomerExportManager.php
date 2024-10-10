235221225d95c58aa11c2ad884bf4bd86d21d244
Telepatrick <eugenekobiliatsky@gmail.com>
2024-10-10 14:44:14 +0300

    CU-8696395vb Skipped export of customers without billing and/or shipping address info (those who not placed any orders, or not filled those data).

--- Model/Export/CustomerExportManager.php
+++ Model/Export/CustomerExportManager.php
@@ -7,6 +7,7 @@ use Magento\Framework\Math\Random;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\Store\Api\StoreConfigManagerInterface;
 use StoreKeeper\StoreKeeper\Helper\Api\Auth;
+use StoreKeeper\StoreKeeper\Logger\Logger;
 
 class CustomerExportManager extends AbstractExportManager
 {
@@ -69,16 +70,19 @@ class CustomerExportManager extends AbstractExportManager
 
     private Random $random;
     private Auth $authHelper;
+    private Logger $logger;
 
     public function __construct(
         Resolver $localeResolver,
         Random $random,
         StoreManagerInterface $storeManager,
         StoreConfigManagerInterface $storeConfigManager,
-        Auth $authHelper
+        Auth $authHelper,
+        Logger $logger
     ) {
         parent::__construct($localeResolver, $storeManager, $storeConfigManager, $authHelper);
         $this->random = $random;
+        $this->logger = $logger;
     }
 
     /**
@@ -91,35 +95,44 @@ class CustomerExportManager extends AbstractExportManager
         foreach ($customers as $customer) {
             $billingAddress = $customer->getDefaultBillingAddress();
             $shippingAddress = $customer->getDefaultShippingAddress();
-            $data = [
-                $this->random->getUniqueHash(), // 'path://shortname' - 'GUID'
-                $this->getCurrentLocale(), // 'path://language_iso2' - 'Language (iso2)'
-                $billingAddress->getCompany(), // 'path://business_data.name'
-                $billingAddress->getCountryId(), // 'path://business_data.country_iso2'
-                $billingAddress->getVatId(), // 'path://business_data.vat_number'
-                $billingAddress->getFirstname(), // 'path://contact_person.firstname'
-                $billingAddress->getLastname(), // 'path://contact_person.familyname'
-                $customer->getEmail(), // 'path://contact_set.email'
-                $billingAddress->getTelephone(), // 'path://contact_set.phone'
-                null, // 'path://contact_set.fax'
-                $shippingAddress->getName(), // 'path://contact_address.name'
-                $shippingAddress->getRegion(), // 'path://contact_address.state'
-                $shippingAddress->getCity(), // 'path://contact_address.city'
-                $shippingAddress->getPostcode(), // 'path://contact_address.zipcode'
-                implode(', ', $shippingAddress->getStreet()), // 'path://contact_address.street'
-                null, // 'path://contact_address.streetnumber'
-                null, // 'path://contact_address.flatnumber'
-                $billingAddress->getCountryId(), // 'path://contact_address.country_iso2'
-                $billingAddress->getName(), // 'path://address_billing.name'
-                $billingAddress->getRegion(), // 'path://address_billing.state'
-                $billingAddress->getCity(), // 'path://address_billing.city'
-                $billingAddress->getPostcode(), // 'path://address_billing.zipcode'
-                implode(', ', $billingAddress->getStreet()), // 'path://address_billing.street'
-                null, // 'path://address_billing.streetnumber'
-                null, // 'path://address_billing.flatnumber'
-                $billingAddress->getCountryId(), // 'path://address_billing.country_iso2'
-            ];
-            $result[] = array_combine(self::HEADERS_PATHS, $data);
+            if ($billingAddress && $shippingAddress) {
+                $data = [
+                    $this->random->getUniqueHash(), // 'path://shortname' - 'GUID'
+                    $this->getCurrentLocale(), // 'path://language_iso2' - 'Language (iso2)'
+                    $billingAddress->getCompany(), // 'path://business_data.name'
+                    $billingAddress->getCountryId(), // 'path://business_data.country_iso2'
+                    $billingAddress->getVatId(), // 'path://business_data.vat_number'
+                    $billingAddress->getFirstname(), // 'path://contact_person.firstname'
+                    $billingAddress->getLastname(), // 'path://contact_person.familyname'
+                    $customer->getEmail(), // 'path://contact_set.email'
+                    $billingAddress->getTelephone(), // 'path://contact_set.phone'
+                    null, // 'path://contact_set.fax'
+                    $shippingAddress->getName(), // 'path://contact_address.name'
+                    $shippingAddress->getRegion(), // 'path://contact_address.state'
+                    $shippingAddress->getCity(), // 'path://contact_address.city'
+                    $shippingAddress->getPostcode(), // 'path://contact_address.zipcode'
+                    implode(', ', $shippingAddress->getStreet()), // 'path://contact_address.street'
+                    null, // 'path://contact_address.streetnumber'
+                    null, // 'path://contact_address.flatnumber'
+                    $billingAddress->getCountryId(), // 'path://contact_address.country_iso2'
+                    $billingAddress->getName(), // 'path://address_billing.name'
+                    $billingAddress->getRegion(), // 'path://address_billing.state'
+                    $billingAddress->getCity(), // 'path://address_billing.city'
+                    $billingAddress->getPostcode(), // 'path://address_billing.zipcode'
+                    implode(', ', $billingAddress->getStreet()), // 'path://address_billing.street'
+                    null, // 'path://address_billing.streetnumber'
+                    null, // 'path://address_billing.flatnumber'
+                    $billingAddress->getCountryId(), // 'path://address_billing.country_iso2'
+                ];
+                $result[] = array_combine(self::HEADERS_PATHS, $data);
+            } else {
+                $this->logger->info(
+                    __(
+                        'Skipped exporting customer due to missing address data, customer id: %1',
+                        $customer->getId()
+                    )
+                );
+            }
         }
 
         return $result;
